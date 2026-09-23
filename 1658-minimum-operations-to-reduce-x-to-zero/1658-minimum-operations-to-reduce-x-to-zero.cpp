0d1679be4696a351817882ee0e150276177ca26f
Z-Suraj <sk8041017@gmail.com>
2026-09-23 23:15:14 +0530

    Time: 4 ms (60.6%), Space: 102.4 MB (38.48%) - LeetHub

---
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int x) {
+        int total = 0;
+
+        // Calculate total sum
+        for (int num : nums) {
+            total += num;
+        }
+
+        // We need to keep a subarray with this sum
+        int target = total - x;
+
+        if (target < 0) {
+            return -1;
+        }
+
+        int left = 0;
+        int sum = 0;
+        int maxLen = -1;
+
+        for (int right = 0; right < nums.size(); right++) {
+            sum += nums[right];
+
+            while (sum > target) {
+                sum -= nums[left];
+                left++;
+            }
+
+            if (sum == target) {
+                maxLen = max(maxLen, right - left + 1);
+            }
+        }
+
+        if (maxLen == -1) {
+            return -1;
+        }
+
+        return nums.size() - maxLen;
+    }
+};
